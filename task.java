14b7774ff915960cd4fb1db3d504472c03855bba
regts <56292969+regts@users.noreply.github.com>
2019-10-24 21:10:03 +0300

    Create task.java

---
+++ task.java
@@ -0,0 +1,24 @@
+public class Main
+{
+	public static void main(String[] args) {
+        int x = 0;
+        while(x < 4){
+            x = x + 1;
+            if(x < 1){
+                System.out.print("a ");
+            }
+        x = x + 2;
+            if (x > 1){
+                System.out.print("annoys");
+                x = x - 1;
+            }
+            if (x == 1){
+                System.out.print("noise");
+            }
+            if(x > 3){
+                System.out.print("an");
+            }
+        System.out.println("");
+        x = x + 1;
+        }
+    }
