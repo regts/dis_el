b1b79ae9c108d9ff891cbfeb0e744a61eeaea1b3
regts <56292969+regts@users.noreply.github.com>
2019-10-24 18:56:53 +0300

    Create numCycle.java

---
+++ numCycle.java
@@ -0,0 +1,14 @@
+public class numCycle
+{
+	public static void main(String[] args) {
+	    int x = 1;
+	    int y = 0;
+	    while(x < 10){
+	        if (x > 3){
+	            y++;
+	            System.out.println("Большой x" + y);
+   }
+        x = x + 1;
+  }
+ }
+}
