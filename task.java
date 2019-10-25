32b22691db1bc838615eda388ab04d4e6d909474
regts <56292969+regts@users.noreply.github.com>
2019-10-25 14:59:03 +0300

    Update task.java

--- task.java
+++ task.java
@@ -3,20 +3,20 @@ public class Main
 	public static void main(String[] args) {
         int x = 0;
         while(x < 4){
-            x = x + 1;
+        System.out.print("a");
             if(x < 1){
-                System.out.print("a ");
+            System.out.print(" ");
             }
-        x = x + 2;
+        System.out.print("n");
             if (x > 1){
-                System.out.print("annoys");
-                x = x - 1;
+            System.out.print(" oyser");
+                x = x + 1;
             }
             if (x == 1){
-                System.out.print("noise");
+            System.out.print("noys");
             }
-            if(x > 3){
-                System.out.print("an");
+            if(x < 1){
+            System.out.print("oise");
             }
         System.out.println("");
         x = x + 1;
