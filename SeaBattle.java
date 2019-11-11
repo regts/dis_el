fea4e9e43a7bcbf06ff895c0afd381fabb54e827
regts <56292969+regts@users.noreply.github.com>
2019-11-11 15:01:31 +0300

    Update SeaBattle.java

--- SeaBattle.java
+++ SeaBattle.java
@@ -1,10 +1,26 @@
-public class SimpleDotComTestDrive {
+public class SimpleDotComGame {
     public static void main(String args[]) {
-        SimpleDotCom dot = new SimpleDotCom();
-        int[] locations = {2, 3, 4};
-        dot.setLocationCells(locations);
-        String userGuess = "2";
-        String result = dot.checkYourself(userGuess);
+       int numOfGuesses = 0;
+       
+       GameHelper helper = new GameHelper();
+       
+       SimpleDotCom theDotCom = new SimpleDotCom();
+       
+       int rendomNum = (int) (Math.random() * 5);
+       
+       int[] locations = {randomNum, randomNum + 1, randomNum + 2};
+       theDotCom.setLocationCells(locations);
+       boolean isAlive = true;
+       
+       while(isAlive == true){
+        String guess = helper.getUserInput("Введите число");
+        String result = theDotCom.checkYourself(guess);
+        numOfGuesses++;
+        if(result.equals("Потопил")){
+            isAlive = false;
+            System.out.println("Потребовалось " + numOfGuesses + " попыток");
+        }
+       }
     }
 }
 class SimpleDotCom{
@@ -30,5 +46,22 @@ class SimpleDotCom{
         }
         System.out.println(result);
         return result;
+    }
+}
+public class GameHelper{
+    public String getUserInput(String prompt){
+        String inputLine = null;
+        System.out.print(prompt + " ");
+        try{
+            BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+            inputLine = is.readLine();
+            if (inputLine.length() == 0){ 
+            return null;
+            
+            catch (IOException e){
+                System.out.println("IOException: " + e);
+            }
+            return inpuLine;
+        }
     }
 }
